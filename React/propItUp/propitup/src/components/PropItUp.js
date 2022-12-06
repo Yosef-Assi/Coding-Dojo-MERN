1265356765b10c5cfa8a4968ce8a5028e8bd67ac
Yosef Assi <yassi3766@gmail.com>
2022-12-06 15:31:23 +0200

    Button

--- React/propItUp/propitup/src/components/PropItUp.js
+++ React/propItUp/propitup/src/components/PropItUp.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 
 export default class PropItUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age,
+        };
+    }
   render() {
     return (
      <div>
       <h2>{this.props.firstName},{this.props.lastName} </h2>
-      <p>{this.props.age}</p>
+      <p>{this.state.age}</p>
       <p>hair Color:{this.props.hairColor}</p>
-      </div>
+      <button onClick={()=>{this.setState({age:this.state.age+1})}}>Birthday Button for {this.props.firstName} {this.props.lastName}
+</button>
+
+       </div>
     )
   }
 }
