781f3f05c75764a49f93bd3cf61de296be84f7ca
Yosef Assi <yassi3766@gmail.com>
2022-12-06 13:52:28 +0200

    React

---
+++ React/test/your-project-name-here/src/components/Com.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import App from '../App.css'
+export  class Com extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <ul className='star'>
+            <li>Learn React</li>
+            <li>Learn React</li>
+            <li>Learn React</li>
+            <li>Learn React</li>
+        </ul>
+
+      </div>
+    )
+  }
+}
+
